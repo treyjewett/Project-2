9a90ee75fec5ae7e84349d1479379c660a101098
Angelee Shaw <angeleefshaw@gmail.com>
2021-02-04 20:36:58 -0500

    tripper_db
    tripper databse schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+
+-- Russell
+INSERT INTO trip (dest_name, dest_continent, dest_summary, dest_rating)
+VALUES('', '', '', '');
+
+-- Will
+INSERT INTO trip (dest_name, dest_continent, dest_summary, dest_rating)
+VALUES('', '', '', '');
+
+-- Josh
+INSERT INTO trip (dest_name, dest_continent, dest_summary, dest_rating)
+VALUES('', '', '', '');
+
+-- Angelee
+INSERT INTO trip (dest_name, dest_continent, dest_summary, dest_rating)
+VALUES('', '', '', '');
