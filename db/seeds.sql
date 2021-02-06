b3bfb452450d346074c0d7f5c35a8917af226305
Angelee Shaw <angeleefshaw@gmail.com>
2021-02-05 22:43:31 -0500

    Update seeds.sql
    added new column and seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,16 +1,24 @@
 
 -- Russell
-INSERT INTO trip (dest_name, dest_continent, dest_summary, dest_rating)
-VALUES('', '', '', '');
+INSERT INTO trip (posted_by, dest_name, dest_summary, dest_rating)
+VALUES('','', '', );
 
 -- Will
-INSERT INTO trip (dest_name, dest_continent, dest_summary, dest_rating)
-VALUES('', '', '', '');
+INSERT INTO trip (posted_by, dest_name, dest_summary, dest_rating)
+VALUES('','', '', );
 
 -- Josh
-INSERT INTO trip (dest_name, dest_continent, dest_summary, dest_rating)
-VALUES('', '', '', '');
+INSERT INTO trip (posted_by, dest_name, dest_summary, dest_rating)
+VALUES('', '', '', );
 
 -- Angelee
-INSERT INTO trip (dest_name, dest_continent, dest_summary, dest_rating)
-VALUES('', '', '', '');
+INSERT INTO trip (posted_by, dest_name, dest_summary, dest_rating)
+VALUES('Angelee', 'Istanbul', 'I visted Istanbul in 2018 after a recommendation from a friend. One thing you will quickly discover is just how vast and expansive the country of Turkey is. Istanbul remains coated in a warm haze, making it an ideal destination for photography at all hours of the day. Street vendors and merchants fill every corner to sell fresh juices, pastries and tea. Walking is no simple task in Istanbul since most streets are very steep and aged.', 10);
+
+-- Brian
+INSERT INTO trip (posted_by, dest_name, dest_summary, dest_rating)
+VALUES('Brian', 'Amsterdam', 'Amsterdam lives and breathes art. I visited to attend Amsterdam Dance Event in 2019. The city itself is dense with a growing population. Transportation varies between biking, running, driving and even sailing. I would reccomend this city to anyone who is craving a new experience at a leisurely pace.', 9);
+
+-- Michelle 
+INSERT INTO trip (posted_by, dest_name, dest_summary, dest_rating)
+VALUES('Anonymous', 'Mt Evans', 'Colorado has grown in popularity over the years for its beauty. The scenic hiking is what attracted me initially in 2016. My favorite experience in Colorado took within the Rocky Mountains on a drive up the 14,000 ft. peak of Mt.Evans. There are several parks and lodges along the way too. I enjoy how new and jagged these mountains appeared once we reached the top.', 7);
