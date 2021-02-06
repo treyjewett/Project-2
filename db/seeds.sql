42c90c5d8e77b9759809ad47e6c67f6ad67eba40
Russell Jewett <rfjewett3@gmail.com>
2021-02-06 14:33:09 -0500

    fixes incorrect data indexing in seeds file.

--- db/seeds.sql
+++ db/seeds.sql
@@ -22,7 +22,7 @@ VALUES('Brian', 'Amsterdam', 'Amsterdam lives and breathes art. I visited to att
 INSERT INTO trip (posted_by, dest_name, dest_summary, dest_rating)
 VALUES('Anonymous', 'Mt Evans', 'Colorado has grown in popularity over the years for its beauty. The scenic hiking is what attracted me initially in 2016. My favorite experience in Colorado took within the Rocky Mountains on a drive up the 14,000 ft. peak of Mt.Evans. There are several parks and lodges along the way too. I enjoy how new and jagged these mountains appeared once we reached the top.', 7);
 
-INSERT INTO trip (dest_name, dest_continent, dest_summary, dest_rating)
+INSERT INTO trip (posted_by, dest_name, dest_summary, dest_rating)
 VALUES('Anonymous', 'France', 'I had always wanted to visit France to see the iconic beauty of Paris. I must say I was a little disappointed. The city had a nasty odor, the people were rude if you do not speak French, and the city as a whole was just not what I was expecting. The French food was underwhelming, and the crowds were just too much for me.', 5);
 
 -- -- Will
@@ -38,9 +38,9 @@ VALUES('Anonymous', 'France', 'I had always wanted to visit France to see the ic
 -- VALUES('', '', '', );
 
 -- Russell
-INSERT INTO trip (dest_name, dest_continent, dest_summary, dest_rating)
+INSERT INTO trip (posted_by, dest_name, dest_summary, dest_rating)
 VALUES('Anonymous', 'Hawaii, USA', 'Hawaii is just gorgeous. The culture is so strong and they are very proud of their heritage. The people are very welcoming and the food is amazing, especially their Taro rolls. If you go, make sure to visit the Polynesian Culture Center, I learned so much about Polynesian culture while I was there.', 8);
 
 -- Michael
-INSERT INTO trip (dest_name, dest_continent, dest_summary, dest_rating)
+INSERT INTO trip (posted_by, dest_name, dest_summary, dest_rating)
 VALUES('Michael', 'Australia', 'Visiting the great Outback had always been on my bucket list, and it exceeded all expectations! Austalians are some of the friendliest people you will ever meet. The landscape is barren for the most part, but there is a certain beauty about the Outback that makes it so unique that you have to see it. And I can not forget the adorable kangaroos!', 9);
