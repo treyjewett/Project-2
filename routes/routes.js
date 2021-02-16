f5c57a9db947669ef5d5b24ceceeabe8f9142510
Russell Jewett <rfjewett3@gmail.com>
2021-02-15 21:21:57 -0500

    gets rid of sort button and all associated code.

--- routes/routes.js
+++ routes/routes.js
@@ -1,84 +1,71 @@
 var axios = require("axios").default;
 var db = require("../models");
 
+
 //home page
-module.exports = function(app) {
-    
-app.get('/', function(req, res) {
-    db.Trip.findAll({
-      raw:true,
-      attributes: ['author', 'name', 'summary', 'rating']
-    }).then(function(data) {
-      //console.log(data)
-      var hbsObject = {trip: data}
-      return res.render('index', hbsObject)
-    })
-});
-
-//addtrip page
-app.get('/addTrip', function(req, res) {
-    res.render("addTrip", {});
-})
-
-//new trip
-app.post('/api/trips', function(req, res) {
-    db.Trip.create({
-        author: req.body.author,
-        name: req.body.name,
-        summary: req.body.summary,
-        rating: req.body.rating
-    }).then(function(dbTrip) {
-        res.json(dbTrip);
-    });
-});
-
-//sort trip
-app.get('/api/trips', function(req, res) {
-  db.Trip.findAll({
-    raw: true,
-    attributes: ['author', 'name', 'summary', 'rating'],
-    order: [
-      ['rating', 'DESC']
-    ]
-  }).then(function(data) {
-    var hbsObject = {trip:data}
-    return res.render('index', hbsObject);
-  })
-})
-
-//delete trip
-app.delete("/api/trips/:summary", function(req, res) {
-  db.Trip.destroy({
-    where: {
-      summary: req.params.summary
+module.exports = function (app) {
+  app.get('/', function (req, res) {
+    var sorted = req.params.sorted;
+    if (!sorted) {
+      db.Trip.findAll({
+        raw: true,
+        attributes: ['author', 'name', 'summary', 'rating']
+      }).then(function (data) {
+        //console.log(data)
+        var hbsObject = { trip: data }
+        return res.render('index', hbsObject)
+      });
     }
-  }).then(function(dbTrip) {
-    res.json(dbTrip);
   });
-});
 
-//pexels api
-app.get('/api/pexels/:query', function(req, res) {
+  //addtrip page
+  app.get('/addTrip', function (req, res) {
+    res.render("addTrip", {});
+  })
+
+  //new trip
+  app.post('/api/trips', function (req, res) {
+    db.Trip.create({
+      author: req.body.author,
+      name: req.body.name,
+      summary: req.body.summary,
+      rating: req.body.rating
+    }).then(function (dbTrip) {
+      res.json(dbTrip);
+    });
+  });
+  
+  //delete trip
+  app.delete("/api/trips/:summary", function (req, res) {
+    db.Trip.destroy({
+      where: {
+        summary: req.params.summary
+      }
+    }).then(function (dbTrip) {
+      res.json(dbTrip);
+    });
+  });
+
+  //pexels api
+  app.get('/api/pexels/:query', function (req, res) {
     const destName = req.params.query
     console.log(destName);
     var options = {
-        method: 'GET',
-        url: 'https://pexelsdimasv1.p.rapidapi.com/v1/search',
-        params: {query: destName, locale: 'en-US', per_page: '15', page: '1'},
-        headers: {
-          authorization: '563492ad6f917000010000014579ed4b2e1848cda35ef6b2313392d5',
-          'x-rapidapi-key': '1eaefda46fmsh7f49637a27e8f45p19f2d6jsn2ebdbc6ee733',
-          'x-rapidapi-host': 'PexelsdimasV1.p.rapidapi.com'
-        }
-      };
-      
+      method: 'GET',
+      url: 'https://pexelsdimasv1.p.rapidapi.com/v1/search',
+      params: { query: destName, locale: 'en-US', per_page: '15', page: '1' },
+      headers: {
+        authorization: '563492ad6f917000010000014579ed4b2e1848cda35ef6b2313392d5',
+        'x-rapidapi-key': '1eaefda46fmsh7f49637a27e8f45p19f2d6jsn2ebdbc6ee733',
+        'x-rapidapi-host': 'PexelsdimasV1.p.rapidapi.com'
+      }
+    };
+
     axios.request(options).then(function (response) {
       res.json(response.data)
-	    //console.log(response.data);
-      }).catch(function (error) {
-	    console.error(error);
+      //console.log(response.data);
+    }).catch(function (error) {
+      console.error(error);
     });
   })
-
-
 }
