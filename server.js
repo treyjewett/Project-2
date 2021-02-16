c503cbf0d9d6d73d58987c600d7645ebe4b2b64b
Angelee Shaw <angeleefshaw@gmail.com>
2021-02-16 13:51:59 -0500

    force false

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.set("view engine", "handlebars");
 require("./routes/routes.js")(app);
 
 //listener
-db.sequelize.sync({force:true}).then(function() {
+db.sequelize.sync({force:false}).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
